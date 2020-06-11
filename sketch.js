47c7df6dc5d9795804c9b022ede6baf05650841a
Roshni <roshnishah318@gmail.com>
2020-06-11 19:09:48 -0400

    fixed audio

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
-var audioClient;
+// var audioClient;
 var scene, camera, renderer, clock, geometry;
+var context;
+var saved;
+
 
 var width = window.innerWidth,
   height = window.innerHeight;
@@ -14,18 +17,71 @@ function setup() {
 function startClicked() {
   document.getElementById("button-c").style.visibility = "hidden";
   init();
-  beginAudioProcessing();
+  loadSound();
 }
 
 // init()
+//-------------------------------------------
+// function beginAudioProcessing() {
+//   var audio = new Audio();
+//   audio.src = 'music.mp3';
+//   audio.controls = true;
+//   audio.autoplay = true;
+//   document.body.appendChild(audio);
+// }
+//-------------above code works but shows music player -------
 
-function beginAudioProcessing() {
-  var audio = new Audio();
-  audio.src = 'music.mp3';
-  audio.controls = true;
-  audio.autoplay = true;
-  document.body.appendChild(audio);
+
+
+try {
+    context = new (window.AudioContext || window.webkitAudioContext)();
 }
+catch (e) {
+    console.log("Your browser doesn't support Web Audio API");
+}
+
+if (saved) {
+    playSound(saved);
+} else {
+    loadSound();
+}
+
+//loading sound into the created audio context
+function loadSound() {
+    //set the audio file's URL
+    var audioURL = 'music.mp3';
+
+    //creating a new request
+    var request = new XMLHttpRequest();
+    request.open('GET', audioURL, true);
+    request.responseType = 'arraybuffer';
+    request.onload = function () {
+        //take the audio from http request and decode it in an audio buffer
+        context.decodeAudioData(request.response, function (buffer) {
+            // save buffer, to not load again
+            saved = buffer;
+            // play sound
+            playSound(buffer);
+        });
+    };
+    request.send();
+}
+
+//playing the audio file
+function playSound(buffer) {
+    //creating source node
+    var source = context.createBufferSource();
+    //passing in data
+    source.buffer = buffer;
+    //giving the source which sound to play
+    source.connect(context.destination);
+    //start playing
+    source.start(0);
+}
+
+//audio playing code ends
+
+
 
 function init() {
 // print("test")
